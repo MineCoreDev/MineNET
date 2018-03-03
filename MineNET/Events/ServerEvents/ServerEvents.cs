be36aa7841fd9c2787aea84b7c125c979d78099e
ひろき <hiroki19990625@gmail.com>
2018-03-03 17:11:24 +0900

    Fix...

--- MineNET/Events/ServerEvents/ServerEvents.cs
+++ MineNET/Events/ServerEvents/ServerEvents.cs
@@ -1,16 +1,14 @@
 ﻿namespace MineNET.Events.ServerEvents
 {
-    public class ServerEvents
+    public class ServerEvents : MineNETEvents
     {
-        public delegate void OnServerStartEventHandler(ServerStartEventArgs args);
-        public static event OnServerStartEventHandler ServerStart;
+        public static event EventHandler<ServerStartEventArgs> ServerStart;
         public static void OnServerStart(ServerStartEventArgs args)
         {
             ServerStart?.Invoke(args);
         }
 
-        public delegate void OnServerStopEventHandler(ServerStopEventArgs args);
-        public static event OnServerStopEventHandler ServerStop;
+        public static event EventHandler<ServerStopEventArgs> ServerStop;
         public static void OnServerStop(ServerStopEventArgs args)
         {
             ServerStop?.Invoke(args);
