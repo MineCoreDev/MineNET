32f9633f5484b575e4e59afbb2badd0766ee6d96
tedo@てど <tedosw1mpr1ver@gmail.com>
2018-03-19 22:16:57 +0900

    Add HelpCommand

---
+++ MineNET/Commands/Defaults/HelpCommand.cs
@@ -0,0 +1,44 @@
+﻿using System.Collections.Generic;
+using MineNET.Utils;
+
+namespace MineNET.Commands.Defaults
+{
+    public class HelpCommand : Command
+    {
+        public override string Name
+        {
+            get
+            {
+                return "help";
+            }
+        }
+
+        public override string Description
+        {
+            get
+            {
+                return "コマンド一覧を表示するコマンド";
+            }
+        }
+
+        public override string[] Aliases
+        {
+            get
+            {
+                return new string[] { "?" };
+            }
+        }
+
+        public override bool Execute(CommandSender sender, params string[] args)
+        {
+            Logger.Info("使えるコマンド一覧");
+            Dictionary<string, Command> commandList = Server.Instance.CommandManager.CommandList;
+            foreach (string name in commandList.Keys)
+            {
+                Command command = commandList[name];
+                Logger.Info($" §2/{command.Name}§f : {command.Description}");
+            }
+            return true;
+        }
+    }
+}
