fbea23044bbf3c0ceefe98d4716ea60d734df6d3
ひろき <hiroki19990625@gmail.com>
2018-03-21 00:36:07 +0900

    Update Files...

--- MineNET/Network/Packets/UpdateBlockPacket.cs
+++ MineNET/Network/Packets/UpdateBlockPacket.cs
@@ -4,11 +4,11 @@ namespace MineNET.Network.Packets
 {
     public class UpdateBlockPacket : DataPacket
     {
-        public const int FLAG_NONE = 0b0000;
-        public const int FLAG_NEIGHBORS = 0b0001;
-        public const int FLAG_NETWORK = 0b0010;
-        public const int FLAG_NOGRAPHIC = 0b0100;
-        public const int FLAG_PRIORITY = 0b1000;
+        public const int FLAG_NONE = 0x0;
+        public const int FLAG_NEIGHBORS = 0x1;
+        public const int FLAG_NETWORK = 0x2;
+        public const int FLAG_NOGRAPHIC = 0x4;
+        public const int FLAG_PRIORITY = 0x8;
 
         public const int FLAG_ALL = UpdateBlockPacket.FLAG_NEIGHBORS | UpdateBlockPacket.FLAG_NETWORK;
         public const int FLAG_ALL_PRIORITY = UpdateBlockPacket.FLAG_ALL | UpdateBlockPacket.FLAG_PRIORITY;
