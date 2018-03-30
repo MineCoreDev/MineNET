a74b644a341012c22cede1f3e107e7dd9e5ab2bb
ひろき <hiroki19990625@gmail.com>
2018-03-30 10:52:02 +0900

    Fix NBTJsonSerializer...

--- MineNET/NBT/IO/NBTJsonSerializer.cs
+++ MineNET/NBT/IO/NBTJsonSerializer.cs
@@ -10,7 +10,6 @@ namespace MineNET.NBT.IO
         public static JObject Serialize(CompoundTag tag)
         {
             JObject json = NBTJsonSerializer.CompoundTagSerialize(tag);
-
             return json;
         }
 
@@ -20,11 +19,7 @@ namespace MineNET.NBT.IO
             foreach (KeyValuePair<string, Tag> kv in tag.Tags)
             {
                 Tag t = kv.Value;
-                if (t is ByteArrayTag)
-                {
-                    json.Add(t.Name, new JArray(tag.GetByteArray(t.Name)));
-                }
-                else if (t is ByteTag)
+                if (t is ByteTag)
                 {
                     json.Add(t.Name, new JValue(tag.GetByte(t.Name)));
                 }
@@ -40,21 +35,13 @@ namespace MineNET.NBT.IO
                 {
                     json.Add(t.Name, new JValue(tag.GetFloat(t.Name)));
                 }
-                else if (t is IntArrayTag)
-                {
-                    json.Add(t.Name, new JArray(tag.GetIntArray(t.Name)));
-                }
                 else if (t is IntTag)
                 {
                     json.Add(t.Name, new JValue(tag.GetInt(t.Name)));
                 }
                 else if (t is ListTag)
                 {
-                    json.Add(t.Name, new JArray());
-                }
-                else if (t is LongArrayTag)
-                {
-                    json.Add(t.Name, new JArray(tag.GetLongArray(t.Name)));
+                    json.Add(t.Name, new JArray(NBTJsonSerializer.ListTagSerialize((ListTag)t)));
                 }
                 else if (t is LongTag)
                 {
@@ -78,11 +65,7 @@ namespace MineNET.NBT.IO
             JArray json = new JArray();
             foreach (Tag t in tag.Tags)
             {
-                if (t is ByteArrayTag)
-                {
-                    json.Add(new JArray(((ByteArrayTag) t).Data));
-                }
-                else if (t is ByteTag)
+                if (t is ByteTag)
                 {
                     json.Add(new JValue(((ByteTag) t).Data));
                 }
@@ -98,10 +81,6 @@ namespace MineNET.NBT.IO
                 {
                     json.Add(new JValue(((FloatTag) t).Data));
                 }
-                else if (t is IntArrayTag)
-                {
-                    json.Add(new JArray(((IntArrayTag) t).Data));
-                }
                 else if (t is IntTag)
                 {
                     json.Add(new JValue(((IntTag) t).Data));
@@ -109,10 +88,6 @@ namespace MineNET.NBT.IO
                 else if (t is ListTag)
                 {
                     json.Add(new JArray(NBTJsonSerializer.ListTagSerialize((ListTag) t)));
-                }
-                else if (t is LongArrayTag)
-                {
-                    json.Add(new JArray(((LongArrayTag) t).Data));
                 }
                 else if (t is LongTag)
                 {
@@ -134,7 +109,6 @@ namespace MineNET.NBT.IO
         public static CompoundTag Deserialize(JObject json)
         {
             CompoundTag tag = NBTJsonSerializer.CompoundTagDeserialize(json);
-
             return tag;
         }
 
@@ -143,125 +117,105 @@ namespace MineNET.NBT.IO
             CompoundTag tag = new CompoundTag();
             foreach (KeyValuePair<string, JToken> kv in json)
             {
-                object t = kv.Value.Value<object>();
-                if (t is byte[])
+                JToken token = kv.Value;
+                if (token is JValue)
                 {
-                    tag.PutByteArray(kv.Key, (byte[]) t);
+                    JValue value = (JValue) token;
+                    object t = value.Value;
+                    if (t is byte)
+                    {
+                        tag.PutByte(kv.Key, (byte) t);
+                    }
+                    else if (t is double)
+                    {
+                        tag.PutDouble(kv.Key, (double) t);
+                    }
+                    else if (t is float)
+                    {
+                        tag.PutFloat(kv.Key, (float) t);
+                    }
+                    else if (t is int)
+                    {
+                        tag.PutInt(kv.Key, (int) t);
+                    }
+                    else if (t is long)
+                    {
+                        tag.PutLong(kv.Key, (long) t);
+                    }
+                    else if (t is short)
+                    {
+                        tag.PutShort(kv.Key, (short) t);
+                    }
+                    else if (t is string)
+                    {
+                        tag.PutString(kv.Key, (string) t);
+                    }
                 }
-                else if (t is byte)
+                else if (token is JObject)
                 {
-                    tag.PutByte(kv.Key, (byte) t);
+                    tag.PutCompound(kv.Key, NBTJsonSerializer.CompoundTagDeserialize((JObject) token));
                 }
-                else if (kv.Value is JObject)
+                else
                 {
-                    tag.PutCompound(kv.Key, NBTJsonSerializer.CompoundTagDeserialize((JObject) kv.Value));
-                }
-                else if (t is double)
-                {
-                    tag.PutDouble(kv.Key, (double) t);
-                }
-                else if (t is float)
-                {
-                    tag.PutFloat(kv.Key, (float) t);
-                }
-                else if (t is int[])
-                {
-                    tag.PutIntArray(kv.Key, (int[]) t);
-                }
-                else if (t is int)
-                {
-                    tag.PutInt(kv.Key, (int) t);
-                }
-                else if (t is long[])
-                {
-                    tag.PutLongArray(kv.Key, (long[]) t);
-                }
-                else if (kv.Value is JArray)
-                {
-                    tag.PutList((ListTag) t);
-                }
-                else if (t is long)
-                {
-                    tag.PutLong(kv.Key, (long) t);
-                }
-                else if (t is short)
-                {
-                    tag.PutShort(kv.Key, (short) t);
-                }
-                else if (t is string)
-                {
-                    tag.PutString(kv.Key, (string) t);
+                    tag.PutList(NBTJsonSerializer.ListTagDeserialize((JArray) token, kv.Key));
                 }
             }
 
             return tag;
         }
 
-        internal static ListTag ListTagDeserialize(JArray json)
+        internal static ListTag ListTagDeserialize(JArray json, string key = "")
         {
-            ListTag tag = new ListTag(NBTTagType.BYTE);
-            foreach (JToken v in json)
+            ListTag tag = new ListTag(key, NBTTagType.BYTE);
+            foreach (JToken token in json)
             {
-                object t = v.Value<object>();
-                if (t is byte[])
+                if (token is JValue)
                 {
-                    tag.ListTagType = NBTTagType.BYTE_ARRAY;
-                    tag.Add(new ByteArrayTag((byte[]) t));
+                    JValue value = (JValue) token;
+                    object t = value.Value;
+                    if (t is byte)
+                    {
+                        tag.ListTagType = NBTTagType.BYTE;
+                        tag.Add(new ByteTag((byte) t));
+                    }
+                    else if (t is double)
+                    {
+                        tag.ListTagType = NBTTagType.DOUBLE;
+                        tag.Add(new DoubleTag((double) t));
+                    }
+                    else if (t is float)
+                    {
+                        tag.ListTagType = NBTTagType.FLOAT;
+                        tag.Add(new FloatTag((float) t));
+                    }
+                    else if (t is int)
+                    {
+                        tag.ListTagType = NBTTagType.INT;
+                        tag.Add(new IntTag((int) t));
+                    }
+                    else if (t is long)
+                    {
+                        tag.ListTagType = NBTTagType.LONG;
+                        tag.Add(new LongTag((long) t));
+                    }
+                    else if (t is short)
+                    {
+                        tag.ListTagType = NBTTagType.SHORT;
+                        tag.Add(new ShortTag((short) t));
+                    }
+                    else if (t is string)
+                    {
+                        tag.ListTagType = NBTTagType.STRING;
+                        tag.Add(new StringTag((string) t));
+                    }
                 }
-                else if (t is byte)
+                else if (token is JObject)
                 {
-                    tag.ListTagType = NBTTagType.BYTE;
-                    tag.Add(new ByteTag((byte) t));
+                    tag.Add(NBTJsonSerializer.CompoundTagDeserialize((JObject) token));
                 }
-                else if (v is JObject)
+                else
                 {
-                    tag.ListTagType = NBTTagType.COMPOUND;
-                    tag.Add(NBTJsonSerializer.CompoundTagDeserialize((JObject) v));
-                }
-                else if (t is double)
-                {
-                    tag.ListTagType = NBTTagType.DOUBLE;
-                    tag.Add(new DoubleTag((double) t));
-                }
-                else if (t is float)
-                {
-                    tag.ListTagType = NBTTagType.FLOAT;
-                    tag.Add(new FloatTag((float) t));
-                }
-                else if (t is int[])
-                {
-                    tag.ListTagType = NBTTagType.INT_ARRAY;
-                    tag.Add(new IntArrayTag((int[]) t));
-                }
-                else if (t is int)
-                {
-                    tag.ListTagType = NBTTagType.INT;
-                    tag.Add(new IntTag((int) t));
-                }
-                else if (t is long[])
-                {
-                    tag.ListTagType = NBTTagType.LONG_ARRAY;
-                    tag.Add(new LongArrayTag((long[]) t));
-                }
-                else if (v is JArray)
-                {
-                    tag.ListTagType = NBTTagType.LIST;
-                    tag.Add(new FloatTag((float) t));
-                }
-                else if (t is long)
-                {
-                    tag.ListTagType = NBTTagType.LONG;
-                    tag.Add(new LongTag((long) t));
-                }
-                else if (t is short)
-                {
-                    tag.ListTagType = NBTTagType.SHORT;
-                    tag.Add(new ShortTag((short) t));
-                }
-                else if (t is string)
-                {
-                    tag.ListTagType = NBTTagType.STRING;
-                    tag.Add(new StringTag((string) t));
+                    tag.Add(NBTJsonSerializer.ListTagDeserialize((JArray) token));
                 }
             }
 
--- MineNETTests/NBT/IO/NBTJsonSerializerTests.cs
+++ MineNETTests/NBT/IO/NBTJsonSerializerTests.cs
@@ -1,6 +1,8 @@
 ﻿using System;
 using Microsoft.VisualStudio.TestTools.UnitTesting;
+using MineNET.NBT.Data;
 using MineNET.NBT.Tags;
+using MineNET.Utils;
 using Newtonsoft.Json.Linq;
 
 namespace MineNET.NBT.IO.Tests
@@ -11,10 +13,25 @@ namespace MineNET.NBT.IO.Tests
         [TestMethod()]
         public void SerializeTest()
         {
+            ListTag list = new ListTag("list", NBTTagType.INT);
+            list.Add(new IntTag(12345));
+            list.Add(new IntTag(67890));
+            CompoundTag subTag = new CompoundTag();
+            subTag.PutBool("bool", true);
+            subTag.PutByte("byte", 123);
             CompoundTag tag = new CompoundTag();
+            tag.PutBool("bool", true);
             tag.PutByte("byte", 123);
-            tag.PutInt("int", 12345);
-            tag.PutLong("long", 123456789);
+            tag.PutByteArray("byteArray", ArrayUtils.CreateArray<byte>(200));
+            tag.PutShort("short", 12345);
+            tag.PutInt("int", 12345678);
+            tag.PutIntArray("intArray", ArrayUtils.CreateArray<int>(200));
+            tag.PutLong("long", 123456789123456);
+            tag.PutLongArray("longArray", ArrayUtils.CreateArray<long>(200));
+            tag.PutFloat("float", 12.3456f);
+            tag.PutDouble("double", 12.3456789);
+            tag.PutList(list);
+            tag.PutCompound("com", subTag);
 
             Console.WriteLine(NBTJsonSerializer.Serialize(tag).ToString());
         }
@@ -22,10 +39,25 @@ namespace MineNET.NBT.IO.Tests
         [TestMethod()]
         public void DeserializeTest()
         {
+            ListTag list = new ListTag("list", NBTTagType.INT);
+            list.Add(new IntTag(12345));
+            list.Add(new IntTag(67890));
+            CompoundTag subTag = new CompoundTag();
+            subTag.PutBool("bool", true);
+            subTag.PutByte("byte", 123);
             CompoundTag tag = new CompoundTag();
+            tag.PutBool("bool", true);
             tag.PutByte("byte", 123);
-            tag.PutInt("int", 12345);
-            tag.PutLong("long", 123456789);
+            tag.PutByteArray("byteArray", ArrayUtils.CreateArray<byte>(200));
+            tag.PutShort("short", 12345);
+            tag.PutInt("int", 12345678);
+            tag.PutIntArray("intArray", ArrayUtils.CreateArray<int>(200));
+            tag.PutLong("long", 123456789123456);
+            tag.PutLongArray("longArray", ArrayUtils.CreateArray<long>(200));
+            tag.PutFloat("float", 12.3456f);
+            tag.PutDouble("double", 12.3456789);
+            tag.PutList(list);
+            tag.PutCompound("com", subTag);
 
             JObject json = NBTJsonSerializer.Serialize(tag);
             Console.WriteLine(NBTJsonSerializer.Serialize(NBTJsonSerializer.Deserialize(json)).ToString());
