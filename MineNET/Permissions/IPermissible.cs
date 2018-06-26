1dc6145bfab0a62d9d3193a24a86be6089ae57e9
tedo@てど <tedosw1mpr1ver@gmail.com>
2018-06-27 00:22:58 +0900

    Add Permissions

---
+++ MineNET/Permissions/IPermissible.cs
@@ -0,0 +1,26 @@
+﻿using System.Collections.Generic;
+using MineNET.Plugins;
+
+namespace MineNET.Permissions
+{
+    public interface IPermissible : IServerOperator
+    {
+        bool IsPermissionSet(string name);
+
+        bool IsPermissionSet(Permission perm);
+
+        bool HasPermission(string name);
+
+        bool HasPermission(Permission perm);
+
+        PermissionAttachment AddAttachment(IPlugin plugin, string name, bool value);
+
+        PermissionAttachment AddAttachment(IPlugin plugin);
+
+        void RemoveAttachment(PermissionAttachment attachment);
+
+        void RecalculatePermissions();
+
+        List<PermissionAttachmentInfo> GetEffectivePermissions();
+    }
+}
---
+++ MineNET/Permissions/PermissionAttachmentInfo.cs
@@ -0,0 +1,18 @@
+﻿namespace MineNET.Permissions
+{
+    public class PermissionAttachmentInfo
+    {
+        public IPermissible Permissible { get; }
+        public string Permission { get; }
+        public PermissionAttachment Attachment { get; }
+        public bool Value { get; }
+
+        public PermissionAttachmentInfo(IPermissible permissible, string permission, PermissionAttachment attachment, bool value)
+        {
+            this.Permissible = permissible;
+            this.Permission = permission;
+            this.Attachment = attachment;
+            this.Value = value;
+        }
+    }
+}
