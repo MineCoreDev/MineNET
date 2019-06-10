012522c022284361c90a32a367776dbb1998cebd
hiroki19990625 <hiroki19990625@gmail.com>
2019-06-10 19:46:33 +0900

    ファイル名を修正

---
+++ MineNET/Network/MinecraftPackets/EntityPickRequestPacket.cs
@@ -0,0 +1,22 @@
+﻿namespace MineNET.Network.MinecraftPackets
+{
+    public class EntityPickRequestPacket : MinecraftPacket
+    {
+        public override byte PacketID { get; } = MinecraftProtocol.ENTITY_PICK_REQUEST_PACKET;
+
+        public long EntityUniqueId { get; set; }
+        public byte HotbarSlot { get; set; }
+
+        protected override void EncodePayload()
+        {
+            this.WriteLLong((ulong) this.EntityUniqueId);
+            this.WriteByte(this.HotbarSlot);
+        }
+
+        protected override void DecodePayload()
+        {
+            this.EntityUniqueId = (long) this.ReadLLong();
+            this.HotbarSlot = this.ReadByte();
+        }
+    }
+}
